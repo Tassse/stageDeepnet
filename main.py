a4945a1559c0a4fa2d7607ed18c5f2283319b43e
Tassse <108277824+Tassse@users.noreply.github.com>
2022-06-30 09:49:19 +0200

    code de l'API

---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import csv
+import json
+
+app = FastAPI()
+
+class Item(BaseModel):
+    light:int
+    temperature:float
+    moisture:int
+    conductivity:int
+    battery:int
+    timestamp:str
+
+
+@app.get("files/{file_path}")
+async def stockage():
+    file = open('file_path', "r")
+    lines = file.readlines()
+    file.close()
+
+    toCSV = []
+    for line in lines:
+        line = line.replace("\n", "")
+        if line != "":
+            line = json.loads(line)
+            toCSV.append(line)
+    keys = toCSV[0].keys()
+    with open('capteur.csv', 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(toCSV)
+
+
+@app.post("/item/")
+async def create_item(item : Item):
+    return item
+
