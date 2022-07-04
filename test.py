ada8d0887e710866f6a428abdffd6a52b4c46bc7
DoleMelipone <91986968+DoleMelipone@users.noreply.github.com>
2022-07-04 10:59:05 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.DataFrame({'name': ['Raphael', 'Donatello'],
+                   'mask': ['red', 'purple'],
+                   'weapon': ['sai', 'bo staff']})
+
+df.to_csv('fichier.csv')
