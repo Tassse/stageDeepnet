6e2c4bdeada87e5ae7f5a93cbc3b80c402d5b2fe
DoleMelipone <91986968+DoleMelipone@users.noreply.github.com>
2022-07-04 16:54:23 +0200

    Update client.py

--- client.py
+++ client.py
@@ -8,7 +8,8 @@ else:
     dodo=sys.argv[1]
     
 data=subprocess.call("miFlora.py", shell=True)
-requete = 'http://127.0.0.1:8000/items/'+data
-r=requests.get(requete)
+#requete = 'http://127.0.0.1:8000/items/'+data
+#r=requests.get(requete)
 #time.sleep(dodo)
 print(dodo)
+print(data)
