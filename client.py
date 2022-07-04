752c99d19739ddc9aa4bdfa85b09063dca8add96
DoleMelipone <91986968+DoleMelipone@users.noreply.github.com>
2022-07-04 16:32:54 +0200

    Update client.py
    Va remplacer boucle.sh

--- client.py
+++ client.py
@@ -1,6 +1,13 @@
 import requests
+import sys
+import miflora.py
 
-requete = 'http://127.0.0.1:8000/items/'+'{"light": 0, "temperature": 21.4, "moisture": 18, "conductivity": 373, "battery": 63, "timestamp": "2022-06-30 08:34:59"}'
-#requete = 'http://127.0.0.1:8000/items/'+miflora.py (il faudra probablement modifier le programme pour que ça marche mais c'est l'idée)
-
-r=requests.get(requete)
+if len(sys.argv==1):
+    dodo=120
+else:
+    dodo=sys.argv[1]
+while true :
+    data=miflora.py
+    requete = 'http://127.0.0.1:8000/items/'+data
+    r=requests.get(requete)
+    time.sleep(dodo)
