cc7189d0484751da2dc9a5f0ecf785bcfa067cdf
DoleMelipone <91986968+DoleMelipone@users.noreply.github.com>
2022-07-04 15:20:41 +0200

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import json
-
-
-itemJson= {"light": 0, "temperature": 20, "moisture": 15, "conductivity": 374, "battery": 63, "timestamp": "2022-06-30 06:58:53"}
-
-df = pd.json_normalize((itemJson))
-df.to_csv('fichier.csv', mode='a', header=False)
