0efcc0e94e6b516078d86f0f94afef407ebb8a89
thais <thais.wuillemin@telecom-sudparis.eu>
2022-07-04 11:33:21 +0200

    ajout du mode 'a' et suppression des header lorsque l'on rajoute quelque chose dans le fichier.csv

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 
 
-itemJson= {"light": 0, "temperature": 21.4, "moisture": 18, "conductivity": 374, "battery": 63, "timestamp": "2022-06-30 06:58:53"}
+itemJson= {"light": 0, "temperature": 20, "moisture": 15, "conductivity": 374, "battery": 63, "timestamp": "2022-06-30 06:58:53"}
 
 df = pd.json_normalize((itemJson))
-df.to_csv('fichier.csv')
+df.to_csv('fichier.csv', mode='a', header=False)
