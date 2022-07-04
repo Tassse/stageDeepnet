59e2954634d8259168e788309b346a3accaf0072
DoleMelipone <91986968+DoleMelipone@users.noreply.github.com>
2022-07-04 16:52:06 +0200

    Autre tentative

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
 import requests
 import sys
-import miFlora.py
+import subprocess
 
 if len(sys.argv==1):
     dodo=120
 else:
     dodo=sys.argv[1]
     
-data=miFlora.py()
+data=subprocess.call("miFlora.py", shell=True)
 requete = 'http://127.0.0.1:8000/items/'+data
 r=requests.get(requete)
 #time.sleep(dodo)
