304adf475994842f069a2c977e629fab433aa435
DoleMelipone <91986968+DoleMelipone@users.noreply.github.com>
2022-07-20 15:53:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
-#from fastapi import FastAPI
-#from pydantic import BaseModel
 import pandas as pd
 import csv
 import json
 from pathlib import Path
-#from fastapi.responses import FileResponse
 from flask import Flask
 import flask
 
 path_capteur_db = Path("database.csv")
 
-#app = FastAPI()
-
 app = Flask(__name__)
 
-"""class Item(BaseModel):
-    light:int
-    temperature:float
-    moisture:int
-    conductivity:int
-    battery:int
-    timestamp:str"""
-
 #itemJson = {"light": 0, "temperature": 21.4, "moisture": 18, "conductivity": 373, "battery": 63, "timestamp": "2022-06-30 08:34:59"}
+
 
 #Version Flask
 
@@ -37,19 +25,3 @@ def update_df(itemJson):
 @app.route("/fichier")
 def fichier():
     return flask.send_file(path_capteur_db)
-
-
-
-#Version FastAPI
-
-"""@app.get("/items/{itemJson}")
-async def update_df(itemJson : str):
-    item = json.loads(itemJson)
-    df = pd.json_normalize((item))
-    #return str(fileObj.absolute())
-    df.to_csv(path_capteur_db,mode='a',header=not(path_capteur_db.is_file()))
-    #return str(path_capteur_db.absolute())
-
-@app.get("/fichier")
-async def fichier():
-    return FileResponse(path_capteur_db)"""
